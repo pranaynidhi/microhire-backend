3c39fa85df27cddb822fa28486746608caf6deb2
pranay <pranay@elitebook.local>
2025-05-29 11:27:52 +0545

    Add File Model with Attributes for File Management

---
+++ models/File.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const File = sequelize.define('File', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  originalName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  filename: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  path: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  mimetype: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  size: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  type: {
+    type: DataTypes.ENUM('resume', 'logo', 'portfolio'),
+    allowNull: false
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id'
+    }
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  tableName: 'files',
+  timestamps: true
+});
+
+module.exports = File;
